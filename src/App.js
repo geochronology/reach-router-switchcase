4cd3b03678c23d354fd102c442af902122ba39ce
geochronology <54387644+geochronology@users.noreply.github.com>
2020-11-09 18:16:49 -0800

    refactored with usecontext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+  memo
+} from 'react'
 import { useWhatChanged } from "@simbathesailor/use-what-changed";
 import { Router, Link, navigate } from "@reach/router";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,29 +13,71 @@ import { Planets, Planet } from './components/Planets'
 import { Starships, Starship } from './components/Starships'
 import './App.css';
 
-function App() {
 
-  // destructure categories from config
-  const { people, planets, starships } = config.categories
+// APP w/ CONTEXT PROVIDER
+export default function App() {
+  return (
+    <StarwarsProvider>
+      <AppContent />
+    </StarwarsProvider>
+  )
+}
+
+
+// CREATE CONTEXT
+const StarwarsContext = createContext()
+
+// CONTEXT PROVIDER
+function StarwarsProvider({ children }) {
+
+  // import categories
+  const categories = config.categories
+
+  // destructure default category of search selection
+  const { people } = categories
 
   // initialize state
-  const [starwarsState, setStarwarsState] = useState(people);
+  const [category, setCategory] = useState(people);
   const [theID, setTheID] = useState('');
 
-  // log updates to ID and starwarsState 
-  useWhatChanged([starwarsState, theID], 'starwarsState, theID')
+
+  return (
+    <StarwarsContext.Provider value={{
+      category,
+      setCategory,
+      theID,
+      setTheID,
+      categories
+    }}>
+      <AppContent />
+    </StarwarsContext.Provider>
+  )
+}
+
+// APP CONTENT
+const AppContent = memo(() => {
+
+  // import global state into component
+  const { category, setCategory } = useContext(StarwarsContext)
+  const { theID, setTheID } = useContext(StarwarsContext)
+
+  // destructure categories
+  const { categories: { people, planets, starships } } = useContext(StarwarsContext)
+
+  // log updates to ID and category 
+  useWhatChanged([category, theID], 'category, theID')
 
   // change state on input from user
   const addId = e => setTheID(e.target.value)
-  const selectCategory = (e) => setStarwarsState(e.target.value)
+  const selectCategory = (e) => setCategory(e.target.value)
 
-  // route the user based on starwarsState
-  useEffect(() => navigate(`/${starwarsState}`), [starwarsState])
+  // route the user based on category
+  useEffect(() => navigate(`/${category}`), [category])
 
   // search swapi based on category and id
   const searchSwapi = e => {
     e.preventDefault()
-    navigate(`/${starwarsState}/${theID}`)
+    navigate(`/${category}/${theID}`)
   }
 
   return (
@@ -63,7 +111,7 @@ function App() {
 
     </div>
   )
-}
+})
 
 const config = {
   categories: {
@@ -72,5 +120,3 @@ const config = {
     starships: 'starships'
   }
 }
-
-export default App;
